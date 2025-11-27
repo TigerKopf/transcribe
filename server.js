d8847026492c4ba279f85139af78bbac1c2bcf19
TigerKopf <79372170+TigerKopf@users.noreply.github.com>
2025-11-27 12:28:56 +0100

    Update server.js

--- server.js
+++ server.js
@@ -19,9 +19,18 @@ const mimeTypes = {
 const listeners = {};
 const stations = {};
 
-const localIp = os.networkInterfaces()['en0']
-  .filter(interface => interface.family === 'IPv4')
-  .map(interface => interface.address)[0];
+const getLocalIp = () => {
+  const nets = os.networkInterfaces();
+  for (const name of Object.keys(nets)) {
+    for (const net of nets[name]) {
+      if (net.family === 'IPv4' && !net.internal) {
+        return net.address;
+      }
+    }
+  }
+  return '127.0.0.1';
+};
+const localIp = getLocalIp();
 
 function createServer(proto, handler) {
   if (proto === 'https') {
